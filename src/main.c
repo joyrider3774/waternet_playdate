f71df77f3eacdb5862c7bc1cdd4f42859e43a3b2
Willems Davy <joyrider3774@hotmail.com>
2023-01-27 00:38:39 +0100

    Add appid for playdate.store

--- src/main.c
+++ src/main.c
@@ -16,6 +16,7 @@ int eventHandler(PlaydateAPI* playdate, PDSystemEvent event, uint32_t arg)
 	if ( event == kEventInit )
 	{
 		setPDPtr(playdate);
+		playdate->system->logToConsole("app.uuid=eabca8a02962448bb4abf53cc07add62");
 		playdate->display->setRefreshRate(FRAMERATE);
 		//original graphcis were 8x8 for a screen resolution of 128x64 (arduboy) i scaled the graphics by 3
 		//so we have some space left and can use a drawoffset to center everything on the screen
